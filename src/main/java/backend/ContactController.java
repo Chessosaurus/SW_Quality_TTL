326e2d9c06f074a7200e38f25009203acf0eb0cf
Timo Zink <zink.timo@protonmail.com>
2024-04-27 10:15:33 +0200

    feat: View

--- src/main/java/backend/ContactController.java
+++ src/main/java/backend/ContactController.java
@@ -1,7 +1,10 @@
 package backend;
 
+import frontend.View;
+
 public class ContactController {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        View home = new View();
     }
 }
